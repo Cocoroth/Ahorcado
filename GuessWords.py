92f2ab03c73096e3d333df03483e6163b26fcada
Cocoroth <alejandro210599@gmail.com>
2024-01-26 11:14:38 +0100

    Añadidas más palabras

--- GuessWords.py
+++ GuessWords.py
@@ -6,5 +6,9 @@ class GuessWords:
         guessWords = ["koala", "calvo", "precipicio", "experimento", "balanza"
                       "tiroteo", "negocios", "borde", "apellido", "marea"
                       "peruano", "turista", "perforar", "hoja", "aplauso"
-                      "torcer", "especialista", "canica", "bostezo", "guirnalda"]
+                      "torcer", "especialista", "canica", "bostezo", "guirnalda"
+                      "especular", "carnicero", "canguro", "sabana", "casco"
+                      "picante", "arroz", "harina", "mapa", "billetes"
+                      "invitacion", "colmena", "anciano", "caminata", "amontonar"
+                      "consulta", "suegra", "elegido", "abanico", "pizarra"]
         return random.choice(guessWords)
